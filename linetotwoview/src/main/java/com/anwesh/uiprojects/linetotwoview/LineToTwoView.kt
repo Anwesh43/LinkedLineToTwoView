ed5052b19552b40a9335cd5188a008ab9c491c91
Anwesh43 <anweshthecool0@gmail.com>
2018-09-11 09:51:55 +0530

    adding LinkedLineToTwo

--- linetotwoview/src/main/java/com/anwesh/uiprojects/linetotwoview/LineToTwoView.kt
+++ linetotwoview/src/main/java/com/anwesh/uiprojects/linetotwoview/LineToTwoView.kt
@@ -144,4 +144,25 @@ class LineToTwoView (ctx : Context) : View(ctx) {
         }
     }
 
+    data class LinkedLineToTwo(var i : Int) {
+        private var curr : LTTNode = LTTNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
