b151f79d8469a72d9f38fd1e491b2a5fa55edc57
Anwesh43 <anweshthecool0@gmail.com>
2018-09-11 09:32:55 +0530

    adding LineToTwoView class

--- linetotwoview/src/main/java/com/anwesh/uiprojects/linetotwoview/LineToTwoView.kt
+++ linetotwoview/src/main/java/com/anwesh/uiprojects/linetotwoview/LineToTwoView.kt
@@ -13,3 +13,20 @@ import android.graphics.Canvas
 
 val nodes : Int = 5
 
+class LineToTwoView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
