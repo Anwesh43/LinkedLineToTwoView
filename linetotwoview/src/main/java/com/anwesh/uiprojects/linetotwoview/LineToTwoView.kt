f3d38816440b3e4ab8cbb0a14ff39beacee71d5b
Anwesh43 <anweshthecool0@gmail.com>
2018-09-11 09:31:05 +0530

    adding libraries and defining number of nodes

--- linetotwoview/src/main/java/com/anwesh/uiprojects/linetotwoview/LineToTwoView.kt
+++ linetotwoview/src/main/java/com/anwesh/uiprojects/linetotwoview/LineToTwoView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.linetotwoview
 /**
  * Created by anweshmishra on 11/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
+
